85ccf692f44ad10a149bea711f259595d34c4221
Prashant love coding <dawkharp79@gmail.com>
2025-07-26 12:14:47 +0530

    Solvedd string palindrom prolem

--- palindrome.js
+++ palindrome.js
@@ -1,21 +1,32 @@
-const num = 12343212;
+const input = 'testset';
 
-function isPalindrome(num) {
+function isPalindrome(input) {
+    if(typeof input === 'string') return isStringPalindrome(input);
+    if(input < 0) return false; // -ve number not a palindrome
 
-    if(num < 0) return false; // -ve number not a palindrome
-
-    let n = num;
+    let n = input;
     let reverseNum = 0;
-
     while(n > 0) {
         const reminder = n %10;
         reverseNum = (10 * reverseNum) + reminder; // to reverse the number
         n = Math.floor(n/10); // Remove the digit after the point
     }
 
-    console.log('reverseNum', reverseNum, num);
-    return reverseNum === num;
+    console.log('reverseNum', reverseNum, input);
+    return reverseNum === input;
 }
 
-console.log('is Palindrome', isPalindrome(num));
+function isStringPalindrome(input) {
+    const inputArray = input.split('');
+    let reverseString = '';
+    for(let i = inputArray.length; i> 0; i--) {
+        reverseString += inputArray[i -1];
+    }
+
+    return reverseString === input;
+}
+
+
+
+console.log('is Palindrome', isPalindrome(input));
 
