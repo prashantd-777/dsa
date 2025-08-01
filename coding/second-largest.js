525d990a687a629ea7e0ca77259c6f9e23b1f5fe
Prashant love coding <dawkharp79@gmail.com>
2025-08-01 22:48:35 +0530

    Handled edge case for second largest element problem

--- coding/second-largest.js
+++ coding/second-largest.js
@@ -1,6 +1,9 @@
 const inputArray = [4, 2, 10, 5, 45, 3];
 
 function getSecondLargestElement(arr) {
+    if(arr.length < 2) {
+        return "Array length should greater than or equal to 2";
+    }
     let largestElement = arr[0];
     let secondLargest = -Infinity;
 
