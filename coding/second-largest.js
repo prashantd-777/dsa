13fbc9ffb2fddbc7b54415526c76acdc1e656f5c
Prashant love coding <dawkharp79@gmail.com>
2025-08-01 22:46:25 +0530

    Solved second largest element from the array

---
+++ coding/second-largest.js
@@ -0,0 +1,19 @@
+const inputArray = [4, 2, 10, 5, 45, 3];
+
+function getSecondLargestElement(arr) {
+    let largestElement = arr[0];
+    let secondLargest = -Infinity;
+
+    for(let i = 1; i< arr.length; i++) {
+        if(arr[i] > largestElement) {
+            secondLargest = largestElement;
+            largestElement = arr[i]
+        } else if(arr[i] > secondLargest && arr[i] < largestElement) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return {largestElement, secondLargest};
+}
+
+console.log('output is::', getSecondLargestElement(inputArray))
