ef192703c72459561b609a0377c75d2969080b10
Prashant love coding <dawkharp79@gmail.com>
2025-08-07 21:46:34 +0530

    Approach 2 : Move zeros to right

--- coding/move-zeros.js
+++ coding/move-zeros.js
@@ -2,8 +2,6 @@
 
 // Note that you must do this in-place without making a copy of the array.
 
- 
-
 // Example 1:
 
 // Input: nums = [0,1,0,3,12]
@@ -28,4 +26,24 @@ function moveZeros(arr) {
     return arr;
 }
 
-console.log('output is::', moveZeros(nums))
+console.log('output is::', moveZeros([...nums]));
+
+
+// Approach 2 => In 1st loop shift only nums to left and in another loop, makes remaining elements are zeros;
+function moveZeros2(nums) {
+    let x = 0;
+    for(let i = 0 ;i< nums.length; i++) {
+        if(nums[i] !== 0) {
+            nums[x] = nums[i];
+            x++;
+        }
+    }
+
+    for(let i = x; i< nums.length; i++) {
+        nums[i] = 0;
+    }
+
+    return nums;
+}
+
+console.log('output 2 is::', moveZeros2([0, 0, 0, 0, 5, 0, 0]));
