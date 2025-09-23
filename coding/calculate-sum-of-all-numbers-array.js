91b077eb0af8e5050268f7234c0cf6f38194a604
Prashant love coding <dawkharp79@gmail.com>
2025-09-23 18:48:21 +0530

    Problem solved: Calculate sum of all odd elements in an array

--- coding/calculate-sum-of-all-numbers-array.js
+++ coding/calculate-sum-of-all-numbers-array.js
@@ -7,4 +7,17 @@ function sum(index) {
     return inputArray[index] + sum(index -1);
 }
 
-console.log('sum is::', sum(inputArray.length -1))
+console.log('sum is::', sum(inputArray.length -1));
+
+
+// calculate sum of all odd elements in an array
+function sumOfOddNumbers(index) {
+    let isOdd = ((inputArray[index] % 2) === 1);
+    if(index === 0) {
+        return isOdd ? inputArray[index] : 0;
+    }
+
+    return ((isOdd ? inputArray[index] : 0) + sumOfOddNumbers(index -1));
+}
+
+console.log('sum of odd numbers', sumOfOddNumbers(inputArray.length - 1));
